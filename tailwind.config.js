996ed4653d1646142b92656bc2778f12e4413693
funerariaelpadrino-svg <funerariaelpadrino@gmail.com>
2025-09-06 22:10:41 -0500

    SEGUNDA

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{html,ts}", // Escanea todos los archivos HTML y TypeScript en src/
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
